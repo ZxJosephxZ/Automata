63121a71afc421fefb8e4f26f04e276d03535ddc
Joseph <josephpajuelo7@gmail.com>
2024-09-30 23:51:32 +0200

    analizador lexico fase1

---
+++ AnalisisLexico/src/Automata.java
@@ -0,0 +1,193 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Automata {
+    private List<Integer> Estados;
+    private List<Character> Alfabeto;
+    private Map<Integer, Map<Character, Integer>> matriz;
+    private Integer estadoInicial;
+    private List<Integer> estadosFinales;
+
+    public Automata(Integer estadoInicial, String estados, String alfabeto, String estadoFinales, List<String> traza) {
+        this.estadoInicial = estadoInicial;
+        this.Estados = iniciarEstados(estados);
+        this.Alfabeto = iniciarAlfabeto(alfabeto);
+        this.estadosFinales = iniciarEstadosFinales(estadoFinales);
+        this.matriz = crearMatriz(Alfabeto, traza);
+    }
+
+    private Map<Character, Integer> creadorTraza(List<Character> caracter, String numero)
+    {
+        int i = 0;
+        Map<Character, Integer> mapeo = new HashMap<>();
+        String[] partes = numero.split(",");
+        while (i < caracter.size())
+        {
+            mapeo.put(caracter.get(i), Integer.parseInt(partes[i]));
+            i++;
+        }
+
+        return mapeo;
+    }
+
+    private Map<Integer, Map<Character, Integer>>  crearMatriz(List<Character> alfabeto, List<String>traza)
+    {
+        //la traza es por indice y cada -> [1,1,2,3,4,5,6] -> que son los parametros para cada letra
+        int i = 0;
+        matriz = new HashMap<>();
+        while (i < getEstados().size())
+        {
+            matriz.put(i, creadorTraza(alfabeto, traza.get(i)));
+            i++;
+        }
+        return matriz;
+    }
+
+    private List<Integer> iniciarEstados(String prompt)
+    {
+        int i = 0;
+        List<Integer> estado = new ArrayList<>();
+        String[] partes = prompt.split(",");
+        while (i < partes.length)
+        {
+            estado.add(Integer.parseInt(partes[i]));
+            i++;
+        }
+        return (estado);
+    }
+    private List<Character> iniciarAlfabeto(String prompt)
+    {
+        int i = 0;
+        List<Character> alfabeto = new ArrayList<>();
+        String[] partes = prompt.split(",");
+        while (i < partes.length)
+        {
+            alfabeto.add(partes[i].charAt(0));
+            i++;
+        }
+        return alfabeto;
+    }
+
+    private List<Integer> iniciarEstadosFinales(String prompt)
+    {
+        int i = 0;
+        List<Integer> finales = new ArrayList<>();
+        String[] partes = prompt.split(",");
+        while (i < partes.length)
+        {
+            finales.add(Integer.parseInt(partes[i]));
+            i++;
+        }
+        return finales;
+    }
+
+    public List<Integer> getEstados() {
+        return Estados;
+    }
+
+    public void setEstados(List<Integer> estados) {
+        Estados = estados;
+    }
+
+    public List<Character> getAlfabeto() {
+        return Alfabeto;
+    }
+
+    public void setAlfabeto(List<Character> alfabeto) {
+        Alfabeto = alfabeto;
+    }
+
+    public Integer getEstadoInicial() {
+        return estadoInicial;
+    }
+
+    public void setEstadoInicial(Integer estadoInicial) {
+        this.estadoInicial = estadoInicial;
+    }
+
+    public List<Integer> getEstadosFinales() {
+        return estadosFinales;
+    }
+
+    public void setEstadosFinales(List<Integer> estadosFinales) {
+        this.estadosFinales = estadosFinales;
+    }
+
+    public Map<Integer, Map<Character, Integer>> getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(Map<Integer, Map<Character, Integer>> matriz) {
+        this.matriz = matriz;
+    }
+
+    public void Operacion(String test)
+    {
+        int i = 0;
+        int control;
+        if (comprobacion(test))
+        {
+            System.out.println("es correcto");
+        }
+        else
+        {
+            System.out.println("incorrecto");
+        }
+        /**
+        int i;
+
+        i = 0;
+        while (i < Test.size() && !Test.isEmpty())
+        {
+            if (comprobacion(Test.get(i)))
+            {
+                System.out.print("primer test completado: ");
+                System.out.println(Test.get(i));
+            }
+            else
+            {
+                System.out.println("no paso");
+            }
+            i++;
+        }*/
+    }
+
+    public boolean contiene(int result)
+    {
+        int i = 0;
+        while (i < estadosFinales.size())
+        {
+            if (estadosFinales.get(i) == result)
+                return true;
+            i++;
+        }
+        return false;
+    }
+
+//comprobar funcionalidad
+    public boolean comprobacion(String test)
+    {
+        int i = 0;
+        int control = 0;
+        String[] partes = test.split(",");
+        while (i < partes.length)
+        {
+            if (control == 0 && matriz.get(estadoInicial).get(partes[i].charAt(0)) != 0)
+            {
+                   control = matriz.get(estadoInicial).get(partes[i].charAt(0));
+            }
+            else if (control != 0 && matriz.get(control).get(partes[i].charAt(0)) != 0)
+            {
+                control = matriz.get(control).get(partes[i].charAt(0));
+            }
+            else
+            {
+                return (contiene(control));
+            }
+            i++;
+        }
+        return true;
+    }
+}
---
+++ AnalisisLexico/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+//q0 = 1 , q1 = 2
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        List<String> trazas = new ArrayList<>();
+        trazas.add("1,2,0,0,0");
+        trazas.add("0,2,0,0,0");
+        trazas.add("0,0,3,0,0");
+        trazas.add("0,0,0,4,0");
+        trazas.add("0,0,0,0,5");
+        trazas.add("0,0,0,4,0");
+        Automata automata = new Automata(0,"1,2,3,4,5,6","a,b,c,d,e","3,5", trazas);
+        //automata.comprobacion();
+        System.out.print("inicial"+automata.getEstadoInicial()+" ");
+        System.out.print("final"+automata.getEstadosFinales().get(0));
+        System.out.println(automata.getEstadosFinales().get(1));
+        System.out.print(" ");
+        System.out.print(automata.getAlfabeto().get(0)+" " );
+        System.out.print(automata.getAlfabeto().get(1)+" " );
+        System.out.print(automata.getAlfabeto().get(2)+" " );
+        System.out.print(automata.getAlfabeto().get(3)+" " );
+        System.out.print(automata.getAlfabeto().get(4)+" " );
+        System.out.println();
+        System.out.println(automata.getEstados().get(0));
+        System.out.println(automata.getEstados().get(1));
+        System.out.println(automata.getEstados().get(2));
+        System.out.println(automata.getEstados().get(3));
+        System.out.println(automata.getEstados().get(4));
+        System.out.println(automata.getEstados().get(5));
+        System.out.println(automata.getMatriz().get(0));
+        System.out.println(automata.getMatriz().get(1));
+        System.out.println(automata.getMatriz().get(2));
+        System.out.println(automata.getMatriz().get(3));
+        System.out.println(automata.getMatriz().get(4));
+        automata.Operacion("a,a,b,e");
+    }
+}
